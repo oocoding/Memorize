3c298f41c4bc882f641d7b033d3000c9d89f7f55
ZhangXingxing <125960373@qq.com>
2021-04-01 00:33:07 +0800

    显示卡片

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ForEach(0 ..< 5) { item in
+                HStack {
+                    ForEach(0 ..< 4) { item in
+                        CardView(isFaceUp: item%2==0)
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
+        }
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10).fill(Color.white)
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(lineWidth: 2)
+            
+            if isFaceUp {
+                Text("🦤")
+            } else {
+                RoundedRectangle(cornerRadius: 10).fill(Color.orange)
+            }
+        }
+        .foregroundColor(.orange)
+        .padding(.all, 6)
     }
 }
---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Zhang Xiangxiong on 2021/4/1.
+//
+
+import Foundation
